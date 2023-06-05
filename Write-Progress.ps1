c96a103201b6aebb32077d1ab49cdb91c1be9f79
Christian Ritter <h.christian.ritter@gmail.com>
2023-06-05 16:05:09 +0200

    Create Write-Progress.ps1
    Basic function without further improvements, so far.

---
+++ Write-Progress.ps1
@@ -0,0 +1,29 @@
+function Write-Progess {
+    [CmdletBinding()]
+    param (
+        $Message
+    )
+    
+    begin {
+        $RotatingSymbol = @("|","/","-","\")
+        $Emptystring +=" " * [Console]::WindowWidth
+    }
+    
+    process {
+        #will make your progressline fancy
+        if($Script:LastIndexSymbol -eq $null){
+            $Script:LastIndexSymbol = 0
+        }
+        
+        Write-Host "`r$emptystring" -NoNewline
+        Write-Host "`r[$($Rotatingsymbol[$($Script:LastIndexSymbol)])]$Message" -NoNewline
+        $Script:LastIndexSymbol+=1
+        if($Script:LastIndexSymbol -eq $RotatingSymbol.Count){
+            $Script:LastIndexSymbol = 0
+        }
+    }
+    
+    end {
+        
+    }
+}
