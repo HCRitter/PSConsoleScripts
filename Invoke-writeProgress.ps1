3714640a3181199f75378f2e19b6f68a53e5454a
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2023-06-05 19:40:54 -0500

    Update Invoke-writeProgress.ps1

--- Invoke-writeProgress.ps1
+++ Invoke-writeProgress.ps1
@@ -1,3 +1,4 @@
+
 function Invoke-writeProgess {
 <#
 .SYNOPSIS
@@ -5,9 +6,10 @@ function Invoke-writeProgess {
 
 .DESCRIPTION
     This function displays a rotating progress message on the console. The rotation is achieved by cycling through two arrays of symbols.
+    Additionally, this function now supports multiple stages of processing messages which change with each rotation of the second symbol.
 
 .PARAMETER Message
-    The message to display next to the rotating symbol.
+    An array of messages to display next to the rotating symbol.
 
 .PARAMETER RotatingSymbol
     The first array of symbols to cycle through for the rotation. If no array is provided, the default symbols are '|', '/', '-', and '\'.
@@ -19,31 +21,34 @@ function Invoke-writeProgess {
     The number of times to cycle through the rotating symbols. If no value is provided, the default is 1.
 
 .EXAMPLE
-    Invoke-writeProgess -Message "Processing..." -RotatingSymbol @('🌑', '🌒', '🌓', '🌔', '🌕', '🌖', '🌗', '🌘') -SecondRotatingSymbol @('❤️', '🧡', '💛', '💚') -LoopCount 2
+    Invoke-writeProgess -Message @('Processing Stage 1...', 'Processing Stage 2...', 'Processing Stage 3...') -RotatingSymbol @('🌑', '🌒', '🌓', '🌔') -SecondRotatingSymbol @('❤️', '🧡', '💛') -LoopCount 2
 
-    This command will display a rotating progress message that cycles through the phases of the moon emoji and number emojis five times.
+    This command will display a rotating progress message that cycles through phases of the moon emojis and heart emojis with the provided messages.
 #>
-
     [CmdletBinding()]
     param (
-        [string]$Message,
-        [string[]]$RotatingSymbol = @("|","/","-","\"),
-        [string[]]$SecondRotatingSymbol = @('❤️', '🧡', '💛', '💚'),
-        [int]$LoopCount = 1
+        [string[]]$Message,  # Array of messages to display
+        [string[]]$RotatingSymbol = @("|","/","-","\"),  # First rotating symbol set
+        [string[]]$SecondRotatingSymbol = @('❤️', '🧡', '💛', '💚', '💙', '💜'),  # Second rotating symbol set
+        [int]$LoopCount = 1  # Number of times to rotate through symbols
     )
     
     begin {
-        $Emptystring = " " * [Console]::WindowWidth
+        $Emptystring = " " * [Console]::WindowWidth  # Create an empty string as wide as the console
     }
     
     process {
+        if ($Message.Count -ne $SecondRotatingSymbol.Count) {
+            throw "Message array and SecondRotatingSymbol array must have the same length."  # Check that the number of messages matches the number of second rotating symbols
+        }
+
         for($k = 0; $k -lt $SecondRotatingSymbol.Count; $k++) {
             for($j = 0; $j -lt $LoopCount; $j++) {
-                #will make your progressline fancy
                 for($i = 0; $i -lt $RotatingSymbol.Count; $i++) {
-                    Write-Host "`r$Emptystring" -NoNewline
-                    Write-Host "`r[$($Rotatingsymbol[$i])$Message $($SecondRotatingsymbol[$k])]" -NoNewline
-                    Start-Sleep -Milliseconds 100
+                    Write-Host "`r$Emptystring" -NoNewline  # Clear the current line
+                    Write-Host "`r[$($Rotatingsymbol[$i]) $($Message[$k].PadRight(20)) $($SecondRotatingsymbol[$k])]" -NoNewline  # Write the current symbol, message, and second symbol
+
+                    Start-Sleep -Milliseconds 100  # Pause before the next iteration
                 }
             }
         }
